52832f741d794ac7a86b145ec572fc6f2a22450a
praveen264 <pstraveen264@gmail.com>
2019-12-24 16:12:19 +0530

    Updates

---
+++ precache-manifest.23c98354462c375ae3a63d7329ad1713.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afae1f96814aa4cf452f735baeacb7a",
+    "url": "/newgithubproject/index.html"
+  },
+  {
+    "revision": "9e0d4f657c728fbb89f9",
+    "url": "/newgithubproject/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c2895a8d038e1f427e02",
+    "url": "/newgithubproject/static/js/2.699cbc3a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newgithubproject/static/js/2.699cbc3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e0d4f657c728fbb89f9",
+    "url": "/newgithubproject/static/js/main.347a1132.chunk.js"
+  },
+  {
+    "revision": "235d166eb0a1c5e7c475",
+    "url": "/newgithubproject/static/js/runtime-main.a9395791.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newgithubproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
